998debc861cc98cf4798e81c7ec8cc5c21e95fc1
Endre Didriksen <endredi@stud.ntnu.no>
2019-10-16 13:05:21 +0200

    wrote pwm module

---
+++ ping_pong_node_22/timer.c
@@ -0,0 +1,8 @@
+/*
+ * timer.c
+ *
+ * Created: 16.10.2019 12:15:16
+ *  Author: endredi
+ */ 
+
+
---
+++ ping_pong_node_22/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ * Created: 16.10.2019 12:15:06
+ *  Author: endredi
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+
+
+
+
+#endif /* TIMER_H_ */
